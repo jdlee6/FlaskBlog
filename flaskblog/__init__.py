b843c87869d6686b0b305fa1e259072a49ca65f3
Joe Lee <jdlee6@buffalo.edu>
2019-05-24 14:46:16 -0400

    user authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '403d1fa75e943fb35a9d537fdfced6ef' 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+# sets the color of our login flash message 
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -12,6 +13,21 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    # Create validate username function with the self, and username argument
+    def validate_username(self, username):
+        # if user is none then it will not the conditional and add the new account to the database
+        user = User.query.filter_by(username=username.data).first()
+        # if user does exist, then it will throw a ValidationError
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    # Create validate email function with the self, and email argument
+    def validate_email(self, email):
+        # if email is none then it will not the conditional and add the new account to the database
+        email = User.query.filter_by(email=email.data).first()
+        # if email does exist, then it will throw a ValidationError
+        if email:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', 
